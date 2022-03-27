bced77789048f87ba38385a26f169b42362e5a55
Szymon Koniecko <80451@student.pb.edu.pl>
2022-03-27 23:17:46 +0200

    zmiany z zad2

---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Zad3.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}."), Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public int? Number { get; set; }
+        public (string, string) getOutput()
+        {
+            string outMessage = "FizzBuzz";
+            string outMethod = "success";
+            if (Number % 3 == 0 && Number % 5 == 0)
+            {
+                return (outMessage, outMethod);
+            }
+            else if (Number % 3 == 0)
+            {
+                outMessage = "Fizz";
+                outMethod = "success";
+                return (outMessage, outMethod);
+            }
+            else if (Number % 5 == 0)
+            {
+                outMessage = "Buzz";
+                outMethod = "success";
+                return (outMessage, outMethod);
+            }
+            else
+            {
+                outMessage = ("Liczba: " + Number + " nie spełnia kryteriów FizzBuzz");
+                outMethod = "warning";
+                return (outMessage, outMethod);
+            }
+
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Zad3.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Zad3.Pages
@@ -6,6 +7,10 @@ namespace Zad3.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,6 +19,15 @@ namespace Zad3.Pages
 
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+        public IActionResult OnPost()
+        {
+            (ViewData["Message"], ViewData["MessageClass"]) = FizzBuzz.getOutput();
+            return Page();
 
         }
     }
